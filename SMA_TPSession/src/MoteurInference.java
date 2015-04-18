b495c0370b9803955b61e594b31d2ee6e5b4265f
Utilisateur <Utilisateur@Utilisateur-PC.uqac.ca>
2015-04-18 19:47:36 -0400

    Creation de la fonction de priorisation

--- SMA_TPSession/src/MoteurInference.java
+++ SMA_TPSession/src/MoteurInference.java
@@ -1,5 +1,67 @@
+import java.util.ArrayList;
+
 /**
  * Created by Florian Sainjeon on 17/04/2015.
  */
 public class MoteurInference {
+	
+	public void Priorite(){
+		
+		for(Aeroport aero :Environnement.aeroports){
+			
+			if(aero.getEnAttenteAtterissage().size()>1){
+				while(aero.getEnAttenteAtterissage()!=null){
+					ArrayList<Avion> nouvelleListe=new ArrayList<>();
+					//Priorite 1:Avarie
+					for(Avion avion: aero.getEnAttenteAtterissage()){
+						if(avion.getEtat()==Avion.Etat.AVARIE){
+							nouvelleListe.add(avion);
+							aero.getEnAttenteAtterissage().remove(avion);
+						}	
+					}
+					//Priorite 2: niveau de carburant
+					int nivBasCarburant=aero.getEnAttenteAtterissage().get(0).getCarburant();
+					Avion tampon=null;
+					while(aero.getEnAttenteAtterissage()!=null){
+						for (Avion avion:aero.getEnAttenteAtterissage()) {
+							// Teste si 2 éléments successifs sont dans le bon ordre ou non
+							if (avion.getCarburant() <= nivBasCarburant) {
+								// s'ils ne le sont pas, on échange leurs positions
+								nivBasCarburant=avion.getCarburant();
+								tampon=avion;
+							}
+						}
+						nouvelleListe.add(tampon);
+						aero.getEnAttenteAtterissage().remove(tampon);
+					}	
+					
+				}
+			}
+		}
+		
+	}
+	
+	public static ArrayList<Avion> triAvionParCarbuantCroissant(ArrayList<Avion> list) {
+		int longueur = list.size();
+		Avion tampon;
+		boolean permut;
+		ArrayList<Avion> nouvelleListe = new ArrayList<>();
+ 
+		do {
+			// hypothèse : l'arraylist est trié
+			permut = false;
+			for (int i = 0; i < longueur - 1; i++) {
+				// Teste si 2 éléments successifs sont dans le bon ordre ou non
+				if (list.get(i).getCarburant() > list.get(i+1).getCarburant()) {
+					// s'ils ne le sont pas, on échange leurs positions
+					tampon = list.get(i);
+					list.remove(list.get(i));
+					list.add(tampon);
+					permut = true;
+				}
+			}
+		} while (permut);
+		return nouvelleListe;
+	}
+	
 }
