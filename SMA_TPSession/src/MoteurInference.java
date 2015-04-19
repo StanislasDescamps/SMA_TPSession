c931824cd5b1fd5c5b191fa392db5b5e9b5632ce
Utilisateur <Utilisateur@Utilisateur-PC.uqac.ca>
2015-04-19 16:28:34 -0400

    Optimisation et creation du nouvel itineraire

--- SMA_TPSession/src/MoteurInference.java
+++ SMA_TPSession/src/MoteurInference.java
@@ -9,44 +9,48 @@ public class MoteurInference {
 		
 		for(Aeroport aero :Environnement.aeroports){
 			ArrayList<Avion> nouvelleListe=new ArrayList<>();
-			if(aero.getEnAttenteAtterissage().size()>1){
-				while(aero.getEnAttenteAtterissage()!=null){
-					
-					//Priorite 1:Avarie
-					for(Avion avion: aero.getEnAttenteAtterissage()){
-						if(avion.getEtat()==Avion.Etat.AVARIE){
-							nouvelleListe.add(avion);
-							aero.getEnAttenteAtterissage().remove(avion);
-						}	
-					}
-					//Priorite 2: niveau de carburant
-					Avion tampon=aero.getEnAttenteAtterissage().get(0);
+			if(aero.getEnAttenteAtterissage().size()!=0){
+				if(aero.getEnAttenteAtterissage().size()>1){
 					while(aero.getEnAttenteAtterissage()!=null){
-						for (Avion avion:aero.getEnAttenteAtterissage()) {
-							// Teste si le niveau de carburant dans le tampon est bien le plus faible
-							if (avion.getCarburant() < tampon.getCarburant()) {
-								// si non on change le tampon pour qu'il devienne le plus faible et sois prioritaire
-								tampon=avion;
-							}
+						
+						//Priorite 1:Avarie
+						for(Avion avion: aero.getEnAttenteAtterissage()){
+							if(avion.getEtat()==Avion.Etat.AVARIE){
+								nouvelleListe.add(avion);
+								aero.getEnAttenteAtterissage().remove(avion);
+							}	
 						}
-						//A la fin de la boucle l'avion avec le carburant le plus faible est ajouter à la nouvelle liste puis supprimer de la liste des attentes atterissage
-						nouvelleListe.add(tampon);
-						aero.getEnAttenteAtterissage().remove(tampon);
-					}	
-					
+						//Priorite 2: niveau de carburant
+						Avion tampon=aero.getEnAttenteAtterissage().get(0);
+						while(aero.getEnAttenteAtterissage()!=null){
+							for (Avion avion:aero.getEnAttenteAtterissage()) {
+								// Teste si le niveau de carburant dans le tampon est bien le plus faible
+								if (avion.getCarburant() < tampon.getCarburant()) {
+									// si non on change le tampon pour qu'il devienne le plus faible et sois prioritaire
+									tampon=avion;
+								}
+							}
+							//A la fin de la boucle l'avion avec le carburant le plus faible est ajouter à la nouvelle liste puis supprimer de la liste des attentes atterissage
+							nouvelleListe.add(tampon);
+							aero.getEnAttenteAtterissage().remove(tampon);
+						}	
+						
+					}
+					//Attribution de la nouvelle liste a la liste d'avion en attente a l'atterissage
+					aero.setEnAttenteAtterissage(nouvelleListe);	
 				}
-				//Attribution de la nouvelle liste a la liste d'avion en attente a l'atterissage
-				aero.setEnAttenteAtterissage(nouvelleListe);
 				//Le premier atteri pret à decoller
 				aero.getEnAttenteDecollage().add(aero.getEnAttenteAtterissage().get(0));
+				
 				//Suppression de ce meme avion de la liste des atterissages
 				aero.getEnAttenteAtterissage().remove(aero.getEnAttenteAtterissage().get(0));
-				System.out.println(aero.getEnAttenteAtterissage().get(0).getNomAgent()+" est bien arrivé à "+aero.getNomAgent()/*+". Il repartira bientot pour "+ Aeroport.nextDestination(aero.getNomAgent())*/);
-			}else if(aero.getEnAttenteAtterissage().size()==1){
-				aero.getEnAttenteDecollage().add(aero.getEnAttenteAtterissage().get(0));
-				//Suppression de ce meme avion de la liste des atterissages
-				aero.getEnAttenteAtterissage().remove(aero.getEnAttenteAtterissage().get(0));
-				System.out.println(aero.getEnAttenteAtterissage().get(0).getNomAgent()+" est bien arrivé à "+aero.getNomAgent()/*+". Il repartira bientot pour "+ Aeroport.nextDestination(aero.getNomAgent())*/);
+				
+				//Choix du nouvel itinéraire pour l'avion
+				Avion avionAuDepart=aero.getEnAttenteAtterissage().get(0);
+				String nextDestination=Aeroport.nextDestination(aero.getNomAgent());
+				avionAuDepart.setScenario(new Scenario(aero.getNomAgent(),nextDestination));
+				
+				System.out.println(avionAuDepart.getNomAgent()+" est bien arrivé à "+aero.getNomAgent()+". Il repartira bientot pour "+ nextDestination);
 			}
 		}
 	}
