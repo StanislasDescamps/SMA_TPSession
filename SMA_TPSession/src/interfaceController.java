111170a3846631f840e33cd7fb2421c1fd45b276
Florian Sainjeon <florian.sainjeon@gmail.com>
2015-04-20 22:15:17 -0400

    Ajout des ID pour les cercles Declaration des cercles dans le controleur

--- SMA_TPSession/src/interfaceController.java
+++ SMA_TPSession/src/interfaceController.java
@@ -1,3 +1,26 @@
-public class interfaceController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.shape.Circle;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class interfaceController implements Initializable {
+    @FXML
+    Circle LIL_BOD_01, LIL_BOD_02, LIL_BOD_03, LIL_BOD_04, LIL_BOD_05, LIL_BOD_06, LIL_BOD_07,
+            ORY_BOD_04, ORY_BOD_03, ORY_BOD_02, ORY_BOD_01,
+            LIL_ORY_02, LIL_ORY_01,
+            LIL_NCE_01, LIL_NCE_02, LIL_NCE_03, LIL_NCE_04, LIL_NCE_05, LIL_NCE_06, LIL_NCE_07,
+            LIL_LYS_01, LIL_LYS_02, LIL_LYS_03, LIL_LYS_04, LIL_LYS_05,
+            ORY_LYS_01, ORY_LYS_02, ORY_LYS_03,
+            ORY_NCE_01, ORY_NCE_02, ORY_NCE_03, ORY_NCE_04, ORY_NCE_05,
+            BOD_LYS_01, BOD_LYS_02, BOD_LYS_03, BOD_LYS_04, BOD_LYS_05, BOD_LYS_06, BOD_LYS_07,
+            BOD_NCE_01, BOD_NCE_02, BOD_NCE_03, BOD_NCE_04, BOD_NCE_05,
+            LYS_NCE_04, LYS_NCE_03, LYS_NCE_02, LYS_NCE_01;
+
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        }
 }
